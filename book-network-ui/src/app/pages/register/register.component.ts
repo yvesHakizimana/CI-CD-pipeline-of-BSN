10e3d4d2bb88ef7b41fa0bea1aa07548b26e70f5
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-06-12 17:28:10 +0200

    Dockerizing both fronted on backend.

---
+++ book-network-ui/src/app/pages/register/register.component.ts
@@ -0,0 +1,45 @@
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthenticationService} from '../../services/services/authentication.service';
+import {RegistrationRequest} from '../../services/models/registration-request';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+
+  registerRequest: RegistrationRequest = {firstName : "", lastName: "", email: "", password: ""};
+  errorMsg: Array<string> = [];
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {
+  }
+
+  login() {
+    this.router.navigate(['login']);
+  }
+
+  register() {
+    this.errorMsg = [];
+    this.authService.register({
+      body: this.registerRequest
+    })
+      .subscribe({
+        next: () => {
+          this.router.navigate(['activate-account']);
+        },
+        error: (err) => {
+          if (err.error.validationErrors != null) {
+            this.errorMsg.push(err.error.validationErrors);
+          } else {
+            this.errorMsg.push(err.error.error);
+          }
+
+        }
+      });
+  }
+}

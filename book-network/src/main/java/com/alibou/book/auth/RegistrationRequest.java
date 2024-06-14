8378ad887e5f3b5b2a4986b25da7785260e5fae4
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-06-14 18:07:05 +0200

    Files responsible for  running pipelines.

---
+++ book-network/src/main/java/com/alibou/book/auth/RegistrationRequest.java
@@ -0,0 +1,31 @@
+package com.alibou.book.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class RegistrationRequest {
+
+
+    @NotEmpty(message = "FirstName is mandatory")
+    @NotNull(message = "FirstName is mandatory")
+    private String firstName;
+    @NotEmpty(message = "LastName is mandatory")
+    @NotNull(message = "LastName is mandatory")
+    private String lastName;
+    @Email(message = "Email is not well formatted")
+    @NotEmpty(message = "Email is mandatory")
+    @NotNull(message = "Email is mandatory")
+    private String email;
+    @NotEmpty(message = "Password is mandatory")
+    @NotNull(message = "Password is mandatory")
+    @Size(min = 8, message = "Password should be 8 characters long minimum")
+    private String password;
+}
